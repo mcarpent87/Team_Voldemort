03440f206d7a7ee8579a3e54c53649812c25722c
Gold Leader <whitneyb18@gmail.com>
2018-09-20 19:07:17 -0600

    Location box sizing fixed answers added to JS

--- assets/JavaScript/WelcomePage.js
+++ assets/JavaScript/WelcomePage.js
@@ -2,17 +2,19 @@
 //possibly need to hide some stuff, just have the welcome page and when they click start, the current page hides and first puzzle load
 //going to need the rest of pages hiding when first page loads, then when user clicks start, front 3rd and 4th pages hide and 2nd page shows
 
-$( document ).ready(function() {
-    $(".containerScoreBoard").hide();
-    $(".container").hide();
-    $("#hideMapRiddle").hide();
-    $("#currency-puzzle-container").hide();
+$(document).ready(function() {
+  $(".containerScoreBoard").hide();
+  $(".container").hide();
+  $("#hideMapRiddle").hide();
+  $("#currency-puzzle-container").hide();
 
-    // $("#show").hide();   example, we can hide rest of page with this before the click function 
-//step one. capture users name
-$(".waves-effect").on("click", function () {
+  // $("#show").hide();   example, we can hide rest of page with this before the click function
+  //step one. capture users name
+  $(".waves-effect").on("click", function() {
     event.preventDefault();
-    var userName = $("#first_name2").val().trim();
+    var userName = $("#first_name2")
+      .val()
+      .trim();
     alert(userName);
     //step two. Hide the rest of the pages, then when user clicks submit, hide front page and show google maps puzzle
     $("#hideMapRiddle").show();
@@ -21,27 +23,26 @@ $(".waves-effect").on("click", function () {
     $(".container").hide();
     //we can add more id's or classes as need to hide/show them
     //then lastly (i think) initialize the clock
-    
-});
-var intervalId;
-    var number = 100;
-    $(".waves-effect").on("click", run);
-        function run() {
-          clearInterval(intervalId);
-          intervalId = setInterval(decrement, 1000);
-        }
-        
-        function decrement() {
-          number--;
-          $("#count").html("<h2>" + "Seconds left: " + number + "</h2>");
-          $("#count2").html("<h2>" + "Seconds left: " + number + "</h2>");
-          $("#count3").html("<h2>" + "Seconds left: " + number + "</h2>");
-          if (number === 0) {
-            stop();
-            alert("Time Up!");
-          }
-        }
-        function stop() {
-            clearInterval(intervalId);
-          }
+  });
+  var intervalId;
+  var number = 100;
+  $(".waves-effect").on("click", run);
+  function run() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+  }
+
+  function decrement() {
+    number--;
+    $("#count").html("<h2>" + "Seconds left: " + number + "</h2>");
+    $("#count2").html("<h2>" + "Seconds left: " + number + "</h2>");
+    $("#count3").html("<h2>" + "Seconds left: " + number + "</h2>");
+    if (number === 0) {
+      stop();
+      alert("Time Up!");
+    }
+  }
+  function stop() {
+    clearInterval(intervalId);
+  }
 });
